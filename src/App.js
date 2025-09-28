a92a37494ffd848d7431466cc0b5558add6f0555
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-28 13:29:49 +0000

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hệ Thống Đánh Giá Chất Lượng</h1>
+      <p>Chào mừng bạn đến với ứng dụng!</p>
+    </div>
+  );
+}
+
+export default App;
