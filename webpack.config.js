250f4fca531cb72d977484fb32503aea4c2576ff
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-28 13:33:12 +0000

    Change output filename to include a hash for better cache management

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
